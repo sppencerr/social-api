72471f4f2c0bd6a843ac149f5d2cd7e19841900d
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-13 02:40:29 -0700

    delete, add reaction, remove reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -65,8 +65,70 @@ const thoughtController = {
                 }
             },
         
+            
+                async deleteThought(req, res) {
+                    try {
+                        const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+                        if (!thought) {
+                            return res.status(404).json({ message: "No thought with this id!" });
+                        }
+            
+                        // Remove thought id from user's `thoughts` field
+                        const user = await User.findOneAndUpdate(
+                            { thoughts: req.params.thoughtId },
+                            { $pull: { thoughts: req.params.thoughtId } },
+                            { new: true }
+                        );
+                        if (!user) {
+                            return res
+                                .status(404)
+                                .json({ message: "Thought created but no user with this id!" });
+                        }
+                        res.json({ message: "Thought deleted!" });
+                    } catch (err) {
+                        console.log(err);
+                        res.status(500).json({ message: "Error deleting thought" });
+                    }
+                },
+                async addReaction(req, res) {
+                    try {
+                        const thought = await Thought.findOneAndUpdate(
+                            { _id: req.params.thoughtId },
+                            { $addToSet: { reactions: req.body } },
+                            { runValidators: true, new: true }
+                        );
+                        if (!thought) {
+                            return res.status(404).json({ message: "No thought with this id!" });
+                        }
+                        res.json(thought);
+                    } catch (err) {
+                        console.log(err);
+                        res.status(500).json({ message: "Error adding reaction" });
+                    }
+                },
+            
+                async removeReaction(req, res) {
+                    try {
+                        const thought = await Thought.findOneAndUpdate(
+                            { _id: req.params.thoughtId },
+                            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                            { runValidators: true, new: true }
+                        );
+                        if (!thought) {
+                            return res.status(404).json({ message: "No thought with this id!" });
+                        }
+                        res.json(thought);
+                    } catch (err) {
+                        console.log(err);
+                        res.status(500).json({ message: "Error removing reaction" });
+                    }
+                },
+            };
+            
+        
+        
+        module.exports = thoughtController
+            
         
 
 
-
-    };
