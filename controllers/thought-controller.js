dc44e57354d75d874265c5b162f4d664a9397aaa
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-13 02:32:09 -0700

    updatethought, create

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -27,4 +27,46 @@ const thoughtController = {
                 res.status(500).json({ message: "Error fetching thought" });
             }
         },
-    
+        // create thought
+        async createThought(req, res) {
+            try {
+                const thought = await Thought.create(req.body);
+                const user = await User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
+                );
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({ message: "Thought created but no user with this id!" });
+                }
+                res.json({ message: "Thought created!" });
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({ message: "Error creating thought" });
+            }
+        },
+       
+            async updateThought(req, res) {
+                try {
+                    const thought = await Thought.findOneAndUpdate(
+                        { _id: req.params.thoughtId },
+                        { $set: req.body },
+                        { runValidators: true, new: true }
+                    );
+                    if (!thought) {
+                        return res.status(404).json({ message: "No thought with this id!" });
+                    }
+                    res.json(thought);
+                } catch (err) {
+                    console.log(err);
+                    res.status(500).json({ message: "Error updating thought" });
+                }
+            },
+        
+        
+
+
+
+    };
