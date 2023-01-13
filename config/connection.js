4b8f29f64cc3074713f441913059d6625460226d
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-13 02:46:51 -0700

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialapi",
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true
+            }
+        );
+        console.log("MongoDB connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
